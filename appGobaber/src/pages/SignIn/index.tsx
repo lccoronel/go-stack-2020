8bde3daf33bc11889b08e8ae736089b23c78dcf0
Lucas Coronel <lucascoronel@MacBook-Pro-de-Lucas.local>
2020-09-21 20:47:04 -0300

    continue page login

--- appGobaber/src/pages/SignIn/index.tsx
+++ appGobaber/src/pages/SignIn/index.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-    </Container>
+    <>
+      <Container>
+        <Image source={logoImg} />
+        <Title>Faça seu logon</Title>
+
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button
+          onPress={() => {
+            console.log('deu');
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
