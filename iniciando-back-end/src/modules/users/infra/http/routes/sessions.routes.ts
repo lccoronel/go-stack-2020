eb2be4b1e8b46a45276aa983da159f5dbb370270
Lucas Coronel <lucascoronel@Lucass-MacBook-Pro.local>
2020-10-05 12:33:08 -0300

    arquitetura DDD ready

--- iniciando-back-end/src/modules/users/infra/http/routes/sessions.routes.ts
+++ iniciando-back-end/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,25 +1,10 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessoinsController = new SessionController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = new AuthenticateUserService();
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json({
-    user,
-    token,
-  });
-});
+sessionsRouter.post('/', sessoinsController.create);
 
 export default sessionsRouter;
