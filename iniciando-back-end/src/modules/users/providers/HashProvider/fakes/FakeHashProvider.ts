6f25e86b060e55ba8a10b9dcce2182357345c0e4
Lucas Coronel <lucascoronel@Lucass-MacBook-Pro.local>
2020-10-08 18:42:15 -0300

    test of authentication  users finished

---
+++ iniciando-back-end/src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -0,0 +1,13 @@
+import IHashProvider from '../models/iHashProvider';
+
+class BcryptHashProvider implements IHashProvider {
+  public async generateHash(payload: string): Promise<string> {
+    return payload;
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return payload === hashed;
+  }
+}
+
+export default BcryptHashProvider;
